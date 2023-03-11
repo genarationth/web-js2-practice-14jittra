5b1b45bf2cb3db394be3231907552d9d668c4753
14jittra <125845796+14jittra@users.noreply.github.com>
2023-03-11 16:15:39 +0700

    Add files via upload

---
+++ challenge1.js
@@ -0,0 +1,5 @@
+let birthYear = 1988;
+let futureYear = 2026;
+let myAge = futureYear - birthYear;
+// Example: If you were born in 1988, then in 2026 you’ll be 37 or 38 (depending on the month in 2026).
+console.log(`If you were born in  ${birthYear} then in ${futureYear} you'll be ${myAge} or ${(myAge - 1)} (depending on the month in ${futureYear} ).`);
---
+++ challenge2.js
@@ -0,0 +1,7 @@
+
+let Name = 'ing';
+let Location = 'thailand';
+let Hobby = 'sleeping';
+//
+console.log(`Hi, my name is ${Name}. I live in ${Location}.and enjoy ${Hobby}.)`);
+ // Example: “Hi, my name is John. I live in South Africa and enjoy drinking homemade wine.”
